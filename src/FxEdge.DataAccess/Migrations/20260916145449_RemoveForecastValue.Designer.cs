892c12f404a2c4910983214d431a2bd96418ea6c
Ehsan-Fakoor <fakur.ehsan@gmail.com>
2026-09-16 18:25:56 +0330

    migration2

--- src/FxEdge.DataAccess/Migrations/20260916145449_RemoveForecastValue.Designer.cs
+++ src/FxEdge.DataAccess/Migrations/20260916145449_RemoveForecastValue.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FxEdge.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FxEdge.DataAccess.Migrations
 {
     [DbContext(typeof(FxEdgeDbContext))]
-    partial class FxEdgeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260916145449_RemoveForecastValue")]
+    partial class RemoveForecastValue
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.12");
---
+++ src/FxEdge.DataAccess/Migrations/20260916145449_RemoveForecastValue.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FxEdge.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveForecastValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ForecastValue",
+                table: "Observations");
+
+            migrationBuilder.DropColumn(
+                name: "BaseActualVsForecast",
+                table: "EntryFundamentalFeatureSnapshots");
+
+            migrationBuilder.DropColumn(
+                name: "BaseForecastValue",
+                table: "EntryFundamentalFeatureSnapshots");
+
+            migrationBuilder.DropColumn(
+                name: "QuoteActualVsForecast",
+                table: "EntryFundamentalFeatureSnapshots");
+
+            migrationBuilder.DropColumn(
+                name: "QuoteForecastValue",
+                table: "EntryFundamentalFeatureSnapshots");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ForecastValue",
+                table: "Observations",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "BaseActualVsForecast",
+                table: "EntryFundamentalFeatureSnapshots",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "BaseForecastValue",
+                table: "EntryFundamentalFeatureSnapshots",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "QuoteActualVsForecast",
+                table: "EntryFundamentalFeatureSnapshots",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "QuoteForecastValue",
+                table: "EntryFundamentalFeatureSnapshots",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
